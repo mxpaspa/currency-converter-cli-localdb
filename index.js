c33bae8b5446ce2c8195f0b354a4945c83935559
mxpaspa <mxpaspa@gmail.com>
2018-08-10 13:24:11 -0400

    refactored login and logs logic

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const prompt = require('prompt')
 const isUserAuthenticated = require('./cmds/login')
 const User = require('./models/userModel')
 const ora = require('ora')
-
+const dbCommands = require('./cmds/db');
 
 module.exports = () => {
+  // function start() {
+  //
+  // }
   const args = minimist(process.argv.slice(2))
 
   let cmd = args._[0] || 'help'
@@ -22,19 +25,14 @@ module.exports = () => {
   }
 
   switch (cmd) {
-    // case 'convert':
-    //   let homeCurrency = args._[1]
-    //   let exchangeCurrency = args._[2]
-    //   let amount = args._[3]
-    //     require('./cmds/convert')(homeCurrency,exchangeCurrency,amount)
-    //     break
 
+//////// please refactor this
     case 'login':
       prompt.start();
       prompt.get(['username', 'password'], function (err, result) {
 
-        var loginUserName = result.username
-        var loginPassword = result.password
+      let loginUserName = result.username
+      let loginPassword = result.password
 
         // console.log('Command-line input received:');
         // console.log('  username: ' + result.username);
@@ -42,66 +40,69 @@ module.exports = () => {
 
         if(loginUserName && loginPassword){
           // define the mlab database url
-          var mongoose = require('mongoose');
-          var uri = 'mongodb://paspam:convertercli12@ds117422.mlab.com:17422/converter-cli';
+          dbCommands.connectDb(function(db){
+            const spinner = ora().start();
 
-          // define a timeout (required for mlab)
-          var options = {
-                "keepAlive" : 300000,
-                "connectTimeoutMS" : 30000,
-                useNewUrlParser: true
+            db.on('error', console.error.bind(console, 'connection error:'));
+              //lOGIN QUERY
+              dbCommands.findUser(User,loginUserName,loginPassword,function(user){
+                 if(user) {
+                     spinner.stop();
+                     console.log(user);
+
+                     prompt.start();
+                     prompt.get(['homecurrency', 'exchangecurrency','amount'], function (err, result) {
+                         var homeCurrency = result.homecurrency
+                         var exchangeCurrency = result.exchangecurrency
+                         var amount = result.amount
+
+                         require('./cmds/convert')(homeCurrency,exchangeCurrency,amount,loginUserName)
+                        //  console.log('Enter C to Continue');
+                        //  prompt.get(['alphabet'],function(err,result){
+                        //    console.log(result);
+                        //  })
+                     });
+
+                 }
+                 else {
+                   console.log("AuthenticationFailed");
+                 }
+              })
+          })
+
           }
-          const spinner = ora().start()
-          // connect to mongodb
-          mongoose.connect(uri, options);
-          var db = mongoose.connection;
-          db.on('error', console.error.bind(console, 'connection error:'));
-
-          User.findOne({username : loginUserName}).then(function(record,isMatch){
-            record.comparePassword(loginPassword,function(err,isMatch){
-              if (err) throw err;
-              spinner.stop();
-              console.log(isMatch);
-
-              prompt.start();
-              prompt.get(['homecurrency', 'exchangecurrency','amount'], function (err, result) {
-                  var homeCurrency = result.homecurrency
-                  var exchangeCurrency = result.exchangecurrency
-                  var amount = result.amount
-                  require('./cmds/convert')(homeCurrency,exchangeCurrency,amount,loginUserName)
-              });
-            })
-          });
-        }
       });
       break
-
+//////// please refactor this
     case 'logs':
     let loginUserName = args._[1]
     let loginPassword = args._[2]
     // define the mlab database url
-    var mongoose = require('mongoose');
-    var uri = 'mongodb://paspam:convertercli12@ds117422.mlab.com:17422/converter-cli';
+    dbCommands.connectDb(function(db){
+      const spinner = ora().start()
+      db.on('error', console.error.bind(console, 'connection error:'));
+      dbCommands.findUser(User,loginUserName,loginPassword,function(user){
 
-    // define a timeout (required for mlab)
-    var options = {
-          "keepAlive" : 300000,
-          "connectTimeoutMS" : 30000,
-          useNewUrlParser: true
-    }
+        if(user) {
+            spinner.stop()
+            require('./cmds/showLogs')(loginUserName)
+        }
+        else {
+          console.log("You are not authenticated");
+        }
+
+
+      })
+    })
 
     // connect to mongodb
-    const spinner = ora().start()
-    mongoose.connect(uri, options);
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
+
+
 
     User.findOne({username : loginUserName}).then(function(record,isMatch){
       record.comparePassword(loginPassword,function(err,isMatch){
         if (err) throw err;
         console.log(isMatch);
-        spinner.stop()
-        require('./cmds/showLogs')(loginUserName)
 
       })
     });
